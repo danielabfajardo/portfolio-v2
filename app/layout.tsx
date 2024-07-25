7a652d95dc67b529caab1e49cee0c135f8d4085d
Daniela B. Fajardo <danielafajardo@ewhain.net>
2024-07-25 13:04:16 -0600

    Google Analytics Integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,13 +2,14 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "./provider";
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Daniela Fajardo - Python, AI, React Software Engineer",
   description: "Software Engineer specializing in Python, AI, React, and NextJS. Full-stack development, data analysis, and app development. Let's build amazing projects together!",
-  keywords: ["software engineer", "python", "AI", "react", "nextjs", "full stack", "frontend", "backend", "data analyst", "aws", "wordpress"],
+  keywords: ["Daniela Fajardo", "software engineer", "python", "AI", "react", "nextjs", "full stack", "frontend", "backend", "data analyst", "aws", "wordpress"],
 };
 
 export default function RootLayout({
@@ -18,6 +19,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Script
+        strategy='lazyOnload'
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_MEASUREMENT_ID}`}
+      />
+
+      <Script id='' strategy='lazyOnload'>
+        {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              
+              gtag('config', '${process.env.NEXT_PUBLIC_MEASUREMENT_ID}', {
+              });
+          `}
+      </Script>
       <body className={inter.className}>
         <ThemeProvider
             attribute="class"
@@ -30,3 +46,4 @@ export default function RootLayout({
     </html>
   );
 }
+
