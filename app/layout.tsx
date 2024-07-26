70f9808853ad4b6f44d20d6fd4a38281e37530b6
Daniela B. Fajardo <danielafajardo@ewhain.net>
2024-07-25 19:07:41 -0600

    Google Analytics bug fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,34 +3,44 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "./provider";
 import Script from 'next/script';
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Daniela Fajardo - Python, AI, React Software Engineer",
+  title: "Daniela Fajardo Website",
   description: "Software Engineer specializing in Python, AI, React, and NextJS. Full-stack development, data analysis, and app development. Let's build amazing projects together!",
   keywords: ["Daniela Fajardo", "software engineer", "python", "AI", "react", "nextjs", "full stack", "frontend", "backend", "data analyst", "aws", "wordpress"],
 };
 
 export default function RootLayout({
   children, 
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   const keywords = Array.isArray(metadata.keywords) ? metadata.keywords.join(', ') : metadata.keywords || '';
 
   return (
     <html lang="en">
-     <head>
+      <Head>
         <meta name="description" content={metadata.description as string} />
         <meta name="keywords" content={keywords} />
         <title>{metadata.title as string}</title>
-
+      </Head>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
         <Script
-          strategy='lazyOnload'
+          strategy='afterInteractive'
           src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_MEASUREMENT_ID}`}
         />
-        <Script id='google-analytics' strategy='lazyOnload'>
+        <Script id='google-analytics' strategy='afterInteractive'>
           {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
@@ -38,17 +48,7 @@ export default function RootLayout({
             gtag('config', '${process.env.NEXT_PUBLIC_MEASUREMENT_ID}');
           `}
         </Script>
-      </head>
-      <body className={inter.className}>
-        <ThemeProvider
-            attribute="class"
-            defaultTheme="dark"
-            enableSystem
-            disableTransitionOnChange
-          >
-            {children}
-          </ThemeProvider></body>
+      </body>
     </html>
   );
 }
-
